3b60970f68eadcfbab85701ef3b87b6b701fb37a
c-manna <60807491+c-manna@users.noreply.github.com>
2022-12-20 20:43:23 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { concatMap, exhaustMap, filter, interval, mergeMap, switchMap, take } from 'rxjs';
+import { combineLatest, concatMap, exhaustMap, filter, forkJoin, interval, mergeMap, of, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -11,11 +11,17 @@ export class AppComponent  implements OnInit{
   constructor(private _http: HttpClient){}
 
   ngOnInit(){
-    let postIds = interval(59).pipe(filter(val=>val>0),take(5));
-    postIds.pipe(exhaustMap((id)=>{
+    let postIds = interval(1000).pipe(filter(val=>val>0),take(5));
+    postIds.pipe(mergeMap((id)=>{
       return this._http.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
     })).subscribe({next:(postDetails)=>{
-      console.log(postDetails)
+      //console.log(postDetails)
     }})
+    forkJoin(postIds).subscribe({next:(data)=>{
+      console.log(data)
+    }})
+    combineLatest(postIds).subscribe({next:(data)=>{
+      console.log(data);
+    }});
   }
 }
