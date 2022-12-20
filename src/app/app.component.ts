3c4ccf92adde26dfd2f84ea53e9b64605550758f
c-manna <60807491+c-manna@users.noreply.github.com>
2022-12-20 20:26:41 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { filter, interval, mergeMap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent  implements OnInit{
+  constructor(private _http: HttpClient){}
+
+  ngOnInit(){
+    let postIds = interval(1000).pipe(filter(val=>val>0),take(5));
+    postIds.pipe(mergeMap((id)=>{
+      return this._http.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    })).subscribe({next:(postDetails)=>{
+      console.log(postDetails)
+    }})
+  }
 }
